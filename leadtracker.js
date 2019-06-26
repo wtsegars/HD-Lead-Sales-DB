dd90d227a7020e30a39a0cbc7fc422a6963b3d7a
Darkangel <wtsegars@mac.com>
2019-06-26 18:30:29 -0400

    Updated the js

--- leadtracker.js
+++ leadtracker.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const userName = ["Admin"];
+const password = ["password"];
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -13,16 +15,47 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if(err) throw err;
     console.log("Connected as ID " + connection.threadId);
+    startApp();
 });
 
-inquirer.prompt([
-    {
-        type: 'password',
-        name: 'loginPassword',
-        message: 'Please Enter Password.'
-    }
-]).then(function(user){
-    if (user.loginPassword === "password"){
+function startApp() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Please Enter Username'
+        },
+        {
+            type: 'password',
+            name: 'loginPassword',
+            message: 'Please Enter Password.'
+        }
+    ]).then(function(user){
+        if (user.username === userName[0]){
+            if (user.loginPassword === passWord[0]){
+                mainMenu();
+            }
+            else {
+                console.log("Password incorrect, please try again.");
+                startApp();
+            }
+        }
+        else {
+            console.log("Username incorrect, please try again.");
+            startApp();
+        }
+    });
+};
+
+function mainMenu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menuOptions',
+            message: 'Choose from the following options:',
+            choices: ["Show Current Lead Status for the Store", "Show Current Lead Status by Department", "Update Lead Goal by Department", "Update Actual Leads by Department", "Add Month"]
+        }
+    ]).then(function(option) {
         
-    }
-})
+    })
+}
